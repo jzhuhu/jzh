48320aa9578d512662ab5705d6f2239559b3938b
jzhuhu <jiazhenhuhu@163.com>
2021-04-25 23:33:17 +0800

    20210424

---
+++ test_20210424/test_20210424/20210424.c
@@ -0,0 +1,110 @@
+//#include<math.h>
+//#include<stdio.h>
+//int main()
+//{
+//	double x, s;
+//	printf("input numble:\n");
+//	scanf_s("%lf", &x);
+//	s = sin(x);
+//	printf("sine of %lf is %lf\n", x, s);
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//int max(int a, int b);
+//int main()
+//{
+//	int x, y, z;
+//	int max(int a, int b);
+//	printf("input two numble:\n");
+//	scanf_s("%d%d", &x, &y);
+//	z = max(x, y);
+//	printf("maxmum=%d", z);
+//
+//
+//	
+//}
+//int max(int a, int b)
+//{
+//	if (a > b)return a; else return b;
+//}
+//#include<stdio.h>
+//#define PRICE 30
+//int main()
+//{
+//	int num, total;
+//	num = 10;
+//	total = num*PRICE;
+//	printf("total=%d", total);
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n", sizeof(unsigned int));
+//	printf("%d\n", sizeof(short int));
+//	printf("%d\n", sizeof(long int));
+//	printf("%d\n", sizeof(long long int));
+//	printf("%d\n", sizeof(char));
+//	//printf("%d\n", sizeof(bool));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	//printf("%d\n", sizeof(int));
+//	//printf("%d\n", sizeof(int));
+//	return 0;
+//
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c, d;
+//	unsigned u;
+//	a = 12; b = -24; u = 10;
+//	c = a + u; d = b + u;
+//	printf("a+u=%d,b+u=%d\n",c, d);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	short int a, b;
+//	a = 32767; 
+//	b = a + 1;
+//	printf("%d,%d\n", a, b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	long x, y;
+//	int a, b, c, d;
+//	x = 5; y = 6; a = 7; b = 8;
+//	c = x + a;
+//	d = y + b;
+//	printf("c=x+a=%d,d=y+b=%d\n", c, d);
+//	return 0;
+//}
+//#include<stdio.h>
+//void main()
+//{
+//	double a, b;
+//	a = 123456.789e5;
+//	b = a + 20;
+//	printf("%f\n", a);
+//	printf("%f\n", b);
+//
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+
+
+	printf("%f\n", 3.0 / 2);
+		return 0;
+}
+
+
+
+
