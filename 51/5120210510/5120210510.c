5b39ceb6a76b155800edbce8e849387aaf3e497c
jzhuhu <jiazhenhuhu@163.com>
2021-05-10 18:38:19 +0800

    0510

---
+++ 51/5120210510/5120210510.c
@@ -0,0 +1,15 @@
+//#include<reg52.h>
+//sbit led=P2^1;
+//void main()
+// {
+// 	   led=0;
+//// 	unsigned int i=0;
+// 	while(1)
+// 	{
+//
+////   for(i=0;i<30000;i++);
+////   led=1;
+////   for(i=0;i<30000;i++);
+//	}
+// }
+
