f81a1375c9f75dfe79c74f05c49805213f837817
jzhuhu <jiazhenhuhu@163.com>
2021-11-16 18:46:21 +0800

    1116

---
+++ c++/20211116/20211116/1116.cpp
@@ -0,0 +1,32 @@
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "jzh" << endl;
+	system("pause");
+	return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 15;
+	cout << "a=" << a << endl;
+	system("pause");
+	return 0;
+}
+*/
+#include<iostream>
+#define day 7
+using namespace std;
+int main()
+{
+	const int mouth = 30;
+	cout << "A week is" << day << endl;
+	cout << "A mouth is " << mouth << endl;
+	system("pause");
+	return 0;
+}
